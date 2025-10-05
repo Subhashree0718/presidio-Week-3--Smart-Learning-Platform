f07c639c6fae75d17f2114bab1df90d73e108beb
Subhashree0718 <158803657+Subhashree0718@users.noreply.github.com>
2025-10-06 00:49:00 +0530

    Update axios.js

--- Frontend/src/api/axios.js
+++ Frontend/src/api/axios.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const BASE_URL = 'http://localhost:4000/api';
+const BASE_URL = 'https://presidio-week-3-smart-learning-platform-1.onrender.com/api';
 export const axiosPublic = axios.create({
     baseURL: BASE_URL,
     headers: { 'Content-Type': 'application/json' },
